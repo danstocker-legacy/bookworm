c461ab1c09800e79d3d0ed58ab3c185a6b8abb3b
Dan Stocker <dan@kwaia.com>
2014-05-17 09:19:31 +0100

    adds library skeleton

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+/*jshint node:true */
+module.exports = function (grunt) {
+    "use strict";
+
+    var params = {
+        files: [
+            'js/namespaces.js',
+            'js/exports.js'
+        ],
+
+        test: [
+            'js/jsTestDriver.conf'
+        ],
+
+        globals: {
+            dessert: true,
+            troop  : true,
+            sntls  : true,
+            evan   : true,
+            flock  : true
+        }
+    };
+
+    // invoking common grunt process
+    require('common-gruntfile')(grunt, params);
+};
---
+++ js/exports.js
@@ -0,0 +1,7 @@
+/**
+ * Library exports
+ */
+/*global dache, module */
+if (typeof module === 'object') {
+    module.exports = dache;
+}
