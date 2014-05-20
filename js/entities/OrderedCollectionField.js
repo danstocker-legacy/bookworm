066681b91343633b2fa0020bfe4bfa4085696fda
Dan Stocker <dan@kwaia.com>
2014-05-20 07:42:21 +0100

    adds basic OrderedCollectionField

--- js/entities/OrderedCollectionField.js
+++ js/entities/OrderedCollectionField.js
@@ -17,6 +17,39 @@ troop.postpone(dache, 'OrderedCollectionField', function () {
      */
     dache.OrderedCollectionField = self
         .addMethods(/** @lends dache.OrderedCollectionField# */{
+            /**
+             * Fetches item order for specified item ID.
+             * @param {string} itemId
+             * @returns {number}
+             */
+            getItemOrder: function (itemId) {
+                var itemKey = this.fieldKey.getItemKey(itemId),
+                    item = itemKey.toItem();
+
+                return itemKey.hasItemMeta() ?
+                    item.getItemMeta('order') :
+                    item.getNode();
+            },
+
+            /**
+             * Retrieves item key from collection matching the specified order.
+             * @param {number} order
+             * @returns {undefined}
+             */
+            getItemKeyByOrder: function (order) {
+                var that = this,
+                    result;
+
+                this.getItemsAsCollection()
+                    .forEachItem(function (item, itemId) {
+                        if (that.getItemOrder(itemId) === order) {
+                            result = that.fieldKey.getItemKey(itemId);
+                            return false;
+                        }
+                    });
+
+                return result;
+            }
         });
 });
 
--- js/entities/OrderedCollectionField.test.js
+++ js/entities/OrderedCollectionField.test.js
@@ -24,4 +24,84 @@
         ok(collection.isA(dache.OrderedCollectionField),
             "should return OrderedCollectionField instance when field type is 'ordered-collection'");
     });
+
+    test("Item order getter", function () {
+        expect(7);
+
+        var orderedCollection = dache.OrderedCollectionField.create('foo/bar/baz'.toFieldKey()),
+            getNodeArgs = [],
+            orderNode = {};
+
+        dache.Item.addMocks({
+            getNode: function () {
+                // called twice
+                equal(this.itemKey.toString(), 'foo/bar/baz/hello', "should fetch item node from cache");
+                getNodeArgs.push(arguments);
+                return orderNode;
+            }
+        });
+
+        dache.ItemKey.addMocks({
+            hasItemMeta: function () {
+                // called twice
+                equal(this.toString(), 'foo/bar/baz/hello', "should test for item meta");
+                return true;
+            }
+        });
+
+        strictEqual(orderedCollection.getItemOrder('hello'), orderNode, "should return order node");
+
+        dache.ItemKey.removeMocks();
+
+        dache.ItemKey.addMocks({
+            hasItemMeta: function () {
+                return false;
+            }
+        });
+
+        strictEqual(orderedCollection.getItemOrder('hello'), orderNode, "should return order node");
+
+        dache.ItemKey.removeMocks();
+
+        dache.Item.removeMocks();
+
+        deepEqual(getNodeArgs, [
+            {0: 'order'},
+            {}
+        ], "should pass correct meta name to getNode");
+    });
+
+    test("Item key getter by order", function () {
+        expect(6);
+
+        var orderedCollection = dache.OrderedCollectionField.create('foo/bar/baz'.toFieldKey()),
+            traversedItemsIds = [],
+            result;
+
+        dache.OrderedCollectionField.addMocks({
+            getItemsAsCollection: function () {
+                // called twice
+                equal(this.fieldKey.toString(), 'foo/bar/baz', "should fetch collection items");
+                return sntls.Collection.create({
+                    'foo': 1,
+                    'bar': 2,
+                    'baz': 3
+                });
+            },
+
+            getItemOrder: function (itemId) {
+                traversedItemsIds.push(itemId);
+                return 0;
+            }
+        });
+
+        equal(typeof orderedCollection.getItemKeyByOrder(3), 'undefined', "should return undefined for invalid order");
+        deepEqual(traversedItemsIds.sort(), ['foo', 'bar', 'baz'].sort(), "should get order until matching order found");
+
+        result = orderedCollection.getItemKeyByOrder(0);
+        ok(result.isA(dache.ItemKey), "should return ItemKey instance");
+        equal(result.itemId, 'foo', "should return ItemKey with matching item ID");
+
+        dache.OrderedCollectionField.removeMocks();
+    });
 }());
