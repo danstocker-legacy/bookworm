6557a666ca706f12b2e6daa9f8b628da07125880
Dan Stocker <dan@kwaia.com>
2014-09-28 19:33:28 +0100

    adds links to documentation of external namespaces

--- js/namespace.js
+++ js/namespace.js
@@ -5,13 +5,29 @@
 /** @namespace */
 var bookworm = {};
 var b$ = bookworm;
-/** @class */
+
+/**
+ * @class
+ * @see https://github.com/production-minds/dessert
+ */
 var dessert = dessert || require('dessert');
-/** @namespace */
+
+/**
+ * @namespace
+ * @see https://github.com/production-minds/troop
+ */
 var troop = troop || require('troop');
-/** @namespace */
+
+/**
+ * @namespace
+ * @see https://github.com/danstocker/sntls
+ */
 var sntls = sntls || require('sntls');
-/** @namespace */
+
+/**
+ * @namespace
+ * @see https://github.com/danstocker/flock
+ */
 var flock = flock || require('flock');
 
 /**
