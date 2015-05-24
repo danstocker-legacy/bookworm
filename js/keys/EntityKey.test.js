733072bd65edbbf6e5283f4328bf1a0f184eebb2
Dan Stocker <dan@kwaia.com>
2015-05-24 14:26:24 +0100

    adds missing EntityKey tests

--- js/keys/EntityKey.test.js
+++ js/keys/EntityKey.test.js
@@ -8,13 +8,17 @@
     test("Instantiation", function () {
         var entityKey = bookworm.EntityKey.create();
 
-       ok(entityKey.hasOwnProperty('subscriptionRegistry'), "should initialize Evented trait");
+        ok(entityKey.hasOwnProperty('subscriptionRegistry'), "should initialize Evented trait");
     });
 
     test("Attribute key getter", function () {
         var documentKey = 'foo/bar'.toDocumentKey(),
             attributeKey;
 
-        attributeKey = documentKey.getAttributeKey()
+        attributeKey = documentKey.getAttributeKey('baz');
+
+        ok(attributeKey.isA(bookworm.AttributeKey), "should return AttributeKey instance");
+        equal(attributeKey.attributeName, 'baz', "should set attributeName property");
+        strictEqual(attributeKey.parentKey, documentKey, "should set parentKey attribute");
     });
 }());
